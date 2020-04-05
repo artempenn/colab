6a24c58bb23c4f667a0f36dea0a5ddf8ba1796af
artempenn <agervets197@gmail.com>
2020-04-05 18:50:29 -0400

    deploy setup with aws

--- provision/terraform/environments/dev/main.tf
+++ provision/terraform/environments/dev/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   version = "2.56"
   profile = "default"
-  region  = "us-east-1"
+  region  = "us-east-2"
 }
 
 terraform {
   backend "s3" {
     bucket = "terraform-b3de3caa-dev"
     key    = "poetry-app"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
 
